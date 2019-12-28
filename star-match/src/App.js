b2daab78183cb9193922a441a82de5dba9ca8a53
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2019-12-28 21:16:39 +0100

    Used utils random and range for replace too many elements

--- star-match/src/App.js
+++ star-match/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
 }
 
 const StarMatch = () => {
+  const [stars, setStars] = useState(utils.random(1,9));
   return (
     <div className="game">
       <div className="help">
@@ -15,26 +16,14 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
+          {utils.range(1, stars).map(starId =>
+            <div key={starId} className="star" /> 
+          )}
         </div>
         <div className="right">
-          <button className="number">1</button>
-          <button className="number">2</button>
-          <button className="number">3</button>
-          <button className="number">4</button>
-          <button className="number">5</button>
-          <button className="number">6</button>
-          <button className="number">7</button>
-          <button className="number">8</button>
-          <button className="number">9</button>
+          {utils.range(1,9).map(number => 
+            <button key={number} className="number">{number}</button>
+          )}
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
